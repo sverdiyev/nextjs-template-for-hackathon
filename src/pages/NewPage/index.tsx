59da8c071275fd990c0806080ec6f03a2940d1b5
sverdiyev <oleksandr.verdiyev@devbridge.com>
2022-11-22 23:05:55 +0100

    add new page

---
+++ src/pages/NewPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index: React.FC = () => {
+  return (
+    <div>
+      <h1>Hey from the new page at link `/NewPage` </h1>
+    </div>
+  );
+};
+
+export default Index;
